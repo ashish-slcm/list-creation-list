628ab26c4a3f8d7adc85b1f1f93ff52113571989
ashish-slcm <ashish.chaudhary@slc-india.com>
2025-05-31 16:11:45 +0530

    Loader and checkbox

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,27 +1,26 @@
+/* === src/components/ListContainer.js === */
 import React from 'react';
 import ListItem from './ListItem';
 import './ListContainer.css';
 
-const ListContainer = ({ title, items, listNumber, onItemAction }) => {
-  return (
-    <div className="list-container">
-      <h2>{title}</h2>
-      <div className="list-scroll-container">
-        {items.length === 0 ? (
-          <p className="empty-text">No items in this list</p>
-        ) : (
-          items.map((item) => (
-            <ListItem
-              key={item.id}
-              item={item}
-              listNumber={listNumber}
-              onItemAction={onItemAction}
-            />
-          ))
+const ListContainer = ({ listNumber, items, isSelectable, isChecked, onCheck, onItemAction }) => (
+  <div className="list-box">
+    <div className='list-header'>
+        {isSelectable && (
+        <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={onCheck}
+            className="list-checkbox"
+        />
         )}
-      </div>
+        <h3>List {listNumber}</h3>
     </div>
-  );
-};
+    
+    {items.map(item => (
+      <ListItem key={item.id} item={item} onItemAction={onItemAction} listNumber={listNumber} />
+    ))}
+  </div>
+);
 
 export default ListContainer;
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import './Loader.css';
 
-const Loader = () => <div className="loader">Loading...</div>;
+const Loader = () => <div className='loader-container'>
+    <div className="loader"></div>
+</div>;
 
 export default Loader;
